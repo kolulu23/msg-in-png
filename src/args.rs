27a34f43d489ee9cbabd49f0fa582c3a2458f1ae
kolulu23 <lancedale775@gmail.com>
2022-03-25 01:29:42 +0800

    Cli implementation

--- src/args.rs
+++ src/args.rs
@@ -1,1 +1,36 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand, AppSettings};
 
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+pub struct Cli {
+    /// Optional name to operate on
+    #[clap(subcommand)]
+    pub command: Command,
+    /// Path to target png file
+    #[clap(short, parse(from_os_str), value_name = "FILE")]
+    pub png: PathBuf,
+
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    /// Add message into a png file
+    Encode {
+        chunk_type: String,
+        message: String,
+        #[clap(short, long, parse(from_os_str))]
+        output: Option<PathBuf>,
+    },
+    /// Get a message from a png file
+    Decode {
+        chunk_type: String,
+    },
+    /// Remove a message from a png file
+    Remove {
+        chunk_type: String,
+    },
+    /// Print given png file
+    Print,
+}
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,55 @@ mod commands;
 mod png;
 mod tests;
 
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter, Read, Seek, Write};
+use std::str::FromStr;
+use crate::args::*;
 use anyhow::Result;
+use clap::{Parser};
+use crate::chunk::Chunk;
+use crate::chunk_type::ChunkType;
+use crate::png::PNG;
 
 fn main() -> Result<()> {
+    let cli: Cli = Cli::parse();
+    println!("{:?} file: {:?}", cli.command, cli.png);
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(false)
+        .open(cli.png.as_path())?;
+    let mut data: Vec<u8> = Vec::with_capacity(file.metadata()?.len() as usize);
+    file.read_to_end(&mut data)?;
+    file.rewind()?;
+    let mut png = PNG::try_from(data.as_slice())?;
+    match cli.command {
+        Command::Encode { chunk_type, message, output } => {
+            let msg_chunk_type = ChunkType::from_str(&chunk_type)?;
+            let msg_chunk = Chunk::new(msg_chunk_type, message.into_bytes());
+            png.append_chunk(msg_chunk);
+            if let Some(output_path) = output {
+                let output_file = File::create(output_path)?;
+                let mut writer = BufWriter::new(output_file);
+                writer.write_all(png.as_bytes().as_slice())?;
+            } else {
+                println!("Trying to overwrite original file: {:?}", cli.png.as_path().canonicalize()?);
+                file.write_all(png.as_bytes().as_slice())?;
+            }
+        }
+        Command::Decode { chunk_type } => {
+            if let Some(msg_chunk) = png.chunk_by_type(&chunk_type) {
+                println!("{}", String::from_utf8(msg_chunk.data().into())?);
+            }
+        }
+        Command::Remove { chunk_type } => {
+            let _msg_chunk = png.remove_chunk(&chunk_type)?;
+            file.write_all(png.as_bytes().as_slice())?;
+            println!("One message of type {} has been removed", chunk_type);
+        }
+        Command::Print => {
+            println!("{:?}", data);
+        }
+    }
     Ok(())
 }
